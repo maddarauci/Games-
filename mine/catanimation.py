d76e9a89e8a33a3b05a95e3b0fbad7f56179b54c
maddarauci <dominicd165@gmail.com>
2022-06-26 14:56:42 +1000

    modified:   mine/catanimation.py 	modified:   Games- (modified content)

--- mine/catanimation.py
+++ mine/catanimation.py
@@ -24,7 +24,33 @@ caty = 10
 direction = 'right'
 
 while True: # the main game loop
-    
+    DISPLAYSURF.fill(WHITE)
+
+    if direction == 'right':
+        catx += 5
+        if catx == 280:
+            direction = 'down'
+    elif direction == 'down':
+        caty += 5
+        if caty == 220:
+            direction = 'left'
+    elif direction == 'left':
+        catx -= 5
+        if catx == 10:
+            direction = 'up'
+    elif direction == 'up':
+        caty -= 5
+        if caty == 10:
+            direction = 'right'
+
+    DISPLAYSURF.blit(catImg, (catx, caty))
+
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.update()
+
 
 
 
