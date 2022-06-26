1ad5ba0b8ee94e86364b643fc703d5fd669600da
maddarauci <dominicd165@gmail.com>
2022-06-26 15:08:40 +1000

    new file:   fonttext.py 	modified:   README.md

---
+++ fonttext.py
@@ -0,0 +1,14 @@
+'''
+Hello World program using pygame's font functions.
+'''
+
+import pygame, sys 
+from pygame.locals import *
+
+pygame.init()
+DISPLAYSURF = pygame.display.set_mode((400, 300))
+pygame.display.set_caption("Hello World!")
+
+WHITE = (255,, 255, 255)
+GREEN = (0, 255, 0)
+BLUE = (0, 0, 128)
