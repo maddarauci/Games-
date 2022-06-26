b820846e92175259220fd4612f6a22a9c3fd09cd
maddarauci <dominicd165@gmail.com>
2022-06-26 14:14:20 +1000

    new file:   mine/catanimation.py

---
+++ mine/catanimation.py
@@ -0,0 +1,19 @@
+'''
+Require assets:
+cat.png
+find at: http://invpy.com/cat.png
+'''
+
+
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+FPS = 30 # frames per second settings
+fpsClock = pygame.time.Clock()
+
+# set up the window
+
+
+print("Hello world, debugging done!!")
