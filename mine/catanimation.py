510044f810ad0b0f38a1ee9b81d3e8da1b02bc9d
maddarauci <dominicd165@gmail.com>
2022-06-26 14:19:24 +1000

    modified:   mine/catanimation.py 	modified:   Games- (modified content)

--- mine/catanimation.py
+++ mine/catanimation.py
@@ -14,6 +14,20 @@ FPS = 30 # frames per second settings
 fpsClock = pygame.time.Clock()
 
 # set up the window
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption("Animation")
+
+WHITE = (255, 255, 255)
+catImg = pygame.image.load('./assets/images/cat.png')
+catx = 10
+caty = 10
+direction = 'right'
+
+while True: # the main game loop
+    
+
+
+
 
 
 print("Hello world, debugging done!!")
